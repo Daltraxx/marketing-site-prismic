6af7dbb10d8b9adf40a2a21754ab6fa3b1b1d2a6
Dalton Pettus <daltpettus@gmail.com>
2025-05-30 14:25:38 -0700

    style Hero section element

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -13,20 +13,26 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
   return (
-    <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-      <PrismicRichText field={slice.primary.heading} components={{
-        heading1: ({ children }) => (
-          <h1 className="text-5xl md:text-7xl font-bold leading-tight tracking-tight font-display text-slate-700">{children}</h1>
-        )
-      }} />
-      <PrismicRichText field={slice.primary.body} components={{
-        paragraph: ({ children }) => (
-          <p className="text-2xl text-center font-body font-normal leading-10 text-slate-600 mb-4 md:mb-8 max-w-md">{children}</p>
-        )
-      }} />
-      <PrismicNextLink field={slice.primary.button_link} className="block w-fit bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wider">
-        {slice.primary.button_text}</PrismicNextLink>
-      <PrismicNextImage field={slice.primary.image} className="drop-shadow-xl max-w-4xl w-full" />
+    <section 
+      data-slice-type={slice.slice_type} 
+      data-slice-variation={slice.variation}
+      className="px-4 py-10 md:py-14 md:px-6 lg:py-16"
+    >
+      <div>
+        <PrismicRichText field={slice.primary.heading} components={{
+          heading1: ({ children }) => (
+            <h1 className="text-5xl md:text-7xl font-bold leading-tight tracking-tight font-display text-slate-700">{children}</h1>
+          )
+        }} />
+        <PrismicRichText field={slice.primary.body} components={{
+          paragraph: ({ children }) => (
+            <p className="text-2xl text-center font-body font-normal leading-10 text-slate-600 mb-4 md:mb-8 max-w-md">{children}</p>
+          )
+        }} />
+        <PrismicNextLink field={slice.primary.button_link} className="block w-fit bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wider">
+          {slice.primary.button_text}</PrismicNextLink>
+        <PrismicNextImage field={slice.primary.image} className="drop-shadow-xl max-w-4xl w-full" />
+      </div>
     </section>
   );
 };
