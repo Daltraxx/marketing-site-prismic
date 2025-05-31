3a809f944626cac550acab50db974f64fcf93c5a
Dalton Pettus <daltpettus@gmail.com>
2025-05-31 11:30:28 -0700

    apply similar treatment to Footer as Header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import Bounded from "@/components//Bounded";
+import Logo from "@/components/Logo";
 
 export default async function Footer() {
    const client = createClient();
@@ -8,17 +10,22 @@ export default async function Footer() {
   const settings = await client.getSingle('settings');
 
    return (
-      <footer>
-         <Link href='/'>{settings.data.site_title}</Link>
-         <small className="block">©{new Date().getFullYear()}</small>
-         <ul>
-            {settings.data.navigation.map(({ link, label }) => (
-               <li key={label}>
-                  <PrismicNextLink field={link}>{label}</PrismicNextLink>
-               </li>
-            ))}
-         </ul>
-         
-      </footer>
+      <Bounded as='footer'>
+         <div className="flex gap-6 items-center justify-between flex-col sm:flex-row">
+            <Link href='/'>
+               <Logo />
+            </Link>
+            <small className="text-xs">©{new Date().getFullYear()}</small>
+            <ul className="flex">
+               {settings.data.navigation.map(({ link, label }) => (
+                  <li key={label}>
+                     <PrismicNextLink field={link} className="p-3">
+                        {label}
+                     </PrismicNextLink>
+                  </li>
+               ))}
+            </ul>
+         </div>
+      </Bounded>
    );
 }
