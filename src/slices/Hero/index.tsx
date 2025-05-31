9c264ddfa808157dcf691a68493aace7c6de422d
Dalton Pettus <daltpettus@gmail.com>
2025-05-30 20:00:54 -0700

    refactor components prop of PrismicRichText elements to be be stored as JSXMapSerializer typed object in components variable

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,9 +1,22 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import Bounded from "@/components/Bounded";
 import Button from "@/components/Button";
+
+const components: JSXMapSerializer = {
+  heading1: ({ children }) => (
+    <h1 className="text-5xl md:text-7xl font-bold leading-tight tracking-tight font-display text-slate-700">
+      {children}
+    </h1>
+  ),
+  paragraph: ({ children }) => (
+    <p className="text-2xl text-center font-body font-normal leading-10 text-slate-600 mb-4 md:mb-8 max-w-md">
+      {children}
+    </p>
+  )
+};
 
 /**
  * Props for `Hero`.
@@ -17,16 +30,8 @@ const Hero: FC<HeroProps> = ({ slice }) => {
   return (
     <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} >
       <div className="grid grid-cols-1 place-items-center text-center">
-        <PrismicRichText field={slice.primary.heading} components={{
-          heading1: ({ children }) => (
-            <h1 className="text-5xl md:text-7xl font-bold leading-tight tracking-tight font-display text-slate-700">{children}</h1>
-          )
-        }} />
-        <PrismicRichText field={slice.primary.body} components={{
-          paragraph: ({ children }) => (
-            <p className="text-2xl text-center font-body font-normal leading-10 text-slate-600 mb-4 md:mb-8 max-w-md">{children}</p>
-          )
-        }} />
+        <PrismicRichText field={slice.primary.heading} components={components} />
+        <PrismicRichText field={slice.primary.body} components={components} />
         <Button field={slice.primary.button_link} className="mb-8 md:mb-10">
           {slice.primary.button_text}
         </Button>
