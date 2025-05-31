1060061a135bce9fdfadf72e6a81a41c23e83a62
Dalton Pettus <daltpettus@gmail.com>
2025-05-31 14:31:06 -0700

    create icons object dictionary for icon components

--- src/slices/Features/index.tsx
+++ src/slices/Features/index.tsx
@@ -18,6 +18,13 @@ const components: JSXMapSerializer = {
   )
 }
 
+const icons = {
+  calendar: <CalendarIcon />,
+  bargraph: <BargraphIcon />,
+  clover: <CloverIcon />,
+  hourglass: <HourglassIcon />
+}
+
 
 /**
  * Props for `Features`.
@@ -47,82 +54,90 @@ const Features: FC<FeaturesProps> = ({ slice }) => {
 export default Features;
 
 function CalendarIcon() {
-  <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="none">
-    <path
-      stroke="#0891B2"
-      strokeWidth="5"
-      d="M6.7 40c0-12.6 0-18.9 3.9-22.8s10.2-3.9 22.7-3.9h13.4c12.5 0 18.8 0 22.7 4 4 3.8 4 10.1 4 22.7v6.7c0 12.5 0 18.8-4 22.7-3.9 4-10.2 4-22.7 4H33.3c-12.5 0-18.8 0-22.7-4-4-3.9-4-10.2-4-22.7V40Z"
-    ></path>
-    <path
-      stroke="#0891B2"
-      strokeLinecap="round"
-      strokeOpacity="0.5"
-      strokeWidth="5"
-      d="M23.3 13.3v-5m33.4 5v-5M8.3 30h63.4"
-    ></path>
-    <path
-      fill="#0891B2"
-      d="M60 56.7a3.3 3.3 0 1 1-6.7 0 3.3 3.3 0 0 1 6.7 0m0-13.4a3.3 3.3 0 1 1-6.7 0 3.3 3.3 0 0 1 6.7 0M43.3 56.7a3.3 3.3 0 1 1-6.6 0 3.3 3.3 0 0 1 6.6 0m0-13.4a3.3 3.3 0 1 1-6.6 0 3.3 3.3 0 0 1 6.6 0M26.7 56.7a3.3 3.3 0 1 1-6.7 0 3.3 3.3 0 0 1 6.7 0m0-13.4a3.3 3.3 0 1 1-6.7 0 3.3 3.3 0 0 1 6.7 0"
-    ></path>
-  </svg>
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="none">
+      <path
+        stroke="#0891B2"
+        strokeWidth="5"
+        d="M6.7 40c0-12.6 0-18.9 3.9-22.8s10.2-3.9 22.7-3.9h13.4c12.5 0 18.8 0 22.7 4 4 3.8 4 10.1 4 22.7v6.7c0 12.5 0 18.8-4 22.7-3.9 4-10.2 4-22.7 4H33.3c-12.5 0-18.8 0-22.7-4-4-3.9-4-10.2-4-22.7V40Z"
+      ></path>
+      <path
+        stroke="#0891B2"
+        strokeLinecap="round"
+        strokeOpacity="0.5"
+        strokeWidth="5"
+        d="M23.3 13.3v-5m33.4 5v-5M8.3 30h63.4"
+      ></path>
+      <path
+        fill="#0891B2"
+        d="M60 56.7a3.3 3.3 0 1 1-6.7 0 3.3 3.3 0 0 1 6.7 0m0-13.4a3.3 3.3 0 1 1-6.7 0 3.3 3.3 0 0 1 6.7 0M43.3 56.7a3.3 3.3 0 1 1-6.6 0 3.3 3.3 0 0 1 6.6 0m0-13.4a3.3 3.3 0 1 1-6.6 0 3.3 3.3 0 0 1 6.6 0M26.7 56.7a3.3 3.3 0 1 1-6.7 0 3.3 3.3 0 0 1 6.7 0m0-13.4a3.3 3.3 0 1 1-6.7 0 3.3 3.3 0 0 1 6.7 0"
+      ></path>
+    </svg>
+  );
 }
 
 function BargraphIcon() {
-  <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="none">
-    <path
-      stroke="#0891B2"
-      strokeLinecap="round"
-      strokeWidth="5"
-      d="M73.3 73.3H6.7"
-    ></path>
-    <path
-      stroke="#0891B2"
-      strokeOpacity="0.5"
-      strokeWidth="5"
-      d="M70 73.3v-25a5 5 0 0 0-5-5H55a5 5 0 0 0-5 5v25"
-    ></path>
-    <path
-      stroke="#0891B2"
-      strokeWidth="5"
-      d="M50 73.3V16.7c0-4.7 0-7.1-1.5-8.6-1.4-1.4-3.8-1.4-8.5-1.4s-7 0-8.5 1.4C30 9.6 30 12 30 16.7v56.6"
-    ></path>
-    <path
-      stroke="#0891B2"
-      strokeOpacity="0.5"
-      strokeWidth="5"
-      d="M30 73.3V31.7a5 5 0 0 0-5-5H15a5 5 0 0 0-5 5v41.6"
-    ></path>
-  </svg>
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="none">
+      <path
+        stroke="#0891B2"
+        strokeLinecap="round"
+        strokeWidth="5"
+        d="M73.3 73.3H6.7"
+      ></path>
+      <path
+        stroke="#0891B2"
+        strokeOpacity="0.5"
+        strokeWidth="5"
+        d="M70 73.3v-25a5 5 0 0 0-5-5H55a5 5 0 0 0-5 5v25"
+      ></path>
+      <path
+        stroke="#0891B2"
+        strokeWidth="5"
+        d="M50 73.3V16.7c0-4.7 0-7.1-1.5-8.6-1.4-1.4-3.8-1.4-8.5-1.4s-7 0-8.5 1.4C30 9.6 30 12 30 16.7v56.6"
+      ></path>
+      <path
+        stroke="#0891B2"
+        strokeOpacity="0.5"
+        strokeWidth="5"
+        d="M30 73.3V31.7a5 5 0 0 0-5-5H15a5 5 0 0 0-5 5v41.6"
+      ></path>
+    </svg>
+  );
 }
 
 function CloverIcon() {
-  <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="none">
-    <path
-      stroke="#0891B2"
-      strokeOpacity="0.5"
-      strokeWidth="5"
-      d="M26.7 26.7h26.6v26.6H26.7z"
-    ></path>
-    <path
-      stroke="#0891B2"
-      strokeWidth="5"
-      d="M53.3 53.3h10a10 10 0 1 1-10 10zm-26.6 0h-10a10 10 0 1 0 10 10zm26.6-26.6h10a10 10 0 1 0-10-10zm-26.6 0h-10a10 10 0 1 1 10-10z"
-    ></path>
-  </svg>
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="none">
+      <path
+        stroke="#0891B2"
+        strokeOpacity="0.5"
+        strokeWidth="5"
+        d="M26.7 26.7h26.6v26.6H26.7z"
+      ></path>
+      <path
+        stroke="#0891B2"
+        strokeWidth="5"
+        d="M53.3 53.3h10a10 10 0 1 1-10 10zm-26.6 0h-10a10 10 0 1 0 10 10zm26.6-26.6h10a10 10 0 1 0-10-10zm-26.6 0h-10a10 10 0 1 1 10-10z"
+      ></path>
+    </svg>
+  );
 }
 
 function HourglassIcon() {
-  <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="none">
-    <path
-      stroke="#0891B2"
-      strokeWidth="5"
-      d="M49.9 30.2 40 40l-9.9-9.8C20.4 20.6 15.5 15.8 17 11.6l.4-1c2-4 9-4 22.7-4 13.8 0 20.7 0 22.7 4l.4 1c1.4 4.2-3.5 9-13.2 18.6Z"
-    ></path>
-    <path
-      stroke="#0891B2"
-      strokeOpacity="0.5"
-      strokeWidth="5"
-      d="M30.1 49.8 40 40l9.9 9.8c9.7 9.6 14.6 14.4 13.2 18.6l-.4 1c-2 4-9 4-22.7 4-13.8 0-20.7 0-22.7-4a7 7 0 0 1-.4-1c-1.4-4.2 3.5-9 13.2-18.6Z"
-    ></path>
-  </svg>
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="none">
+      <path
+        stroke="#0891B2"
+        strokeWidth="5"
+        d="M49.9 30.2 40 40l-9.9-9.8C20.4 20.6 15.5 15.8 17 11.6l.4-1c2-4 9-4 22.7-4 13.8 0 20.7 0 22.7 4l.4 1c1.4 4.2-3.5 9-13.2 18.6Z"
+      ></path>
+      <path
+        stroke="#0891B2"
+        strokeOpacity="0.5"
+        strokeWidth="5"
+        d="M30.1 49.8 40 40l9.9 9.8c9.7 9.6 14.6 14.4 13.2 18.6l-.4 1c-2 4-9 4-22.7 4-13.8 0-20.7 0-22.7-4a7 7 0 0 1-.4-1c-1.4-4.2 3.5-9 13.2-18.6Z"
+      ></path>
+    </svg>
+  );
 }
