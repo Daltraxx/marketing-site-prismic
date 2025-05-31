90e318071cdd2367622841df7e558ca1f0cb9958
Dalton Pettus <daltpettus@gmail.com>
2025-05-31 09:45:10 -0700

    style horizontal Hero variation

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -14,7 +14,7 @@ const components: JSXMapSerializer = {
   ),
   // create component for paragraph?
   paragraph: ({ children }) => (
-    <p className="text-2xl text-center font-body font-normal leading-10 text-slate-600 mb-4 md:mb-8 max-w-md">
+    <p className="text-2xl font-body font-normal leading-10 text-slate-600 mb-4 md:mb-8 max-w-md">
       {children}
     </p>
   )
@@ -46,12 +46,14 @@ const Hero: FC<HeroProps> = ({ slice }) => {
 
     {slice.variation === 'horizontal' && (
       <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} >
-        <div className="grid grid-cols-1 place-items-center text-center">
-          <PrismicRichText field={slice.primary.heading} components={components} />
-          <PrismicRichText field={slice.primary.body} components={components} />
-          <Button field={slice.primary.button_link} className="mb-8 md:mb-10">
-            {slice.primary.button_text}
-          </Button>
+        <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
+          <div className="grid grid-rows-[1fr,auto,auto] h-fit">
+            <PrismicRichText field={slice.primary.heading} components={components} />
+            <PrismicRichText field={slice.primary.body} components={components} />
+            <Button field={slice.primary.button_link} className="mb-8 md:mb-10">
+              {slice.primary.button_text}
+            </Button>
+          </div>
           <PrismicNextImage field={slice.primary.image} className="drop-shadow-xl max-w-4xl w-full" />
         </div>
       </Bounded>
